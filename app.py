d114a19f90f5e18b6c349939ef173597efaad2be
crberry50 <crberry@us.ibm.com>
2023-03-07 15:34:25 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>CBs Hello, World ! - Pyflask Demo 2</h1>'
+    return '<h1>CBs Hello, World Too ! - Pyflask Demo 2</h1>'
 
 @app.route('/version')
 def get_version():
-    return '<h1>App version : <b>4.1</b></h1>'
+    return '<h1>App version : <b>4.2</b></h1>'
 
 @app.route('/test')
 def get_test():
-    return '<h1>You are accessing /test endpoint again</h1>'
+    return '<h1>You too are accessing /test endpoint again</h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
